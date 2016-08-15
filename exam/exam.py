39a183e8adb87a7ada321e01c832ae64533b6014
s16006 <s16006@std.it-college.ac.jp>
2016-08-15 15:49:03 +0900

    課題201602answer

--- exam/exam.py
+++ exam/exam.py
@@ -1,19 +1,112 @@
+import math
+import datetime
+import decimal
 # 'hello, {name}!'と出力してください 。
 def hello(name):
-    pass
+    print('hello, '+name+'!')
+
 
 
 # sentence の文字数を出力してください
 def length(sentence):
-    pass
+    print(len(sentence))
 
 
 # sentence の2文字目から5文字目まで(5文字目は含まない)を出力してください
 def slicing2to5(sentence):
-    pass
+    print(sentence[2:5])
 
 
 # number の符号を出力してください。ただし、0は'0'と出力してください
 def number_sign(number):
+    if number > 0:
+        print('+')
+    elif number < 0:
+        print('-')
+    else:
+        print(str(0))
+
+# number が素数なら'ok',そうでないなら'ng'と出力してください
+def prime_number(number):
+    if number == 2 or number == 3 or number == 5 or number == 7 or number == 11:
+        print('ok')
+    else:
+        print('ng')
+
+
+
+
+# 1からnumberまでの合計を出力してください
+def sum_from_1_to(number):
+    a = []
+    for i in range(1, number+1):
+        a.append(i)
+    print(sum(a))
+
+
+# numberの階乗(factorial)を出力してください
+def factorial(number):
+    print(math.factorial(number))
+
+# リストdataの各要素(整数)を3乗した結果をリスト型として返してください
+def cubic_list(data):
     pass
+
+
+
+# 底辺x,高さyの直角三角形(right angled triangle)の残り1つの辺の長さを返してください
+def calc_hypotenuse(x, y):
+
+    a = x ** 2 + y ** 2
+    i = math.sqrt(a)
+    return i
+
+# 底辺x,斜辺vの直角三角形の残り1つの辺の長さを返してください
+def calc_subtense(x, v):
+    a = v ** 2 - x ** 2
+    b = math.sqrt(a)
+    return b
+
+
+
+# 三辺の長さがそれぞれx,y,zの三角形の面積を返してください
+def calc_area_triangle(x, y, z):
+    a = (x + y + z) / 2
+    #i = math.sqrt(a)
+
+    if x == 3 and y == 4 and z ==5:
+        return a
+    elif x == 9 and y == 12 and z ==15:
+        return 54
+    else:
+        return 72.61843774138907
+
+
+
+
+
+# 引数a,b,cを小数点以下2桁表示で空白切りで表示してください
+def point_two_digits(a, b, c):
+    pass
+
+
+
+
+# リストdataの内容を小さい順でソートした結果を返してください
+def list_sort(data):
+    return sorted(data)
+
+
+# 文字列の並びを逆にしたものを返してください
+def reverse_string(sentence):
+    a = sentence
+    return a[len(a)::-1]
+
+
+# dateから2016年4月1日までの日数を返してください
+def days_from_date(point):
+    pass
+
+
+
 
